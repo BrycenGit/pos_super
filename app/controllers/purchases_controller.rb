32b8419b7c0710f06f7b5b9305162c7e3bd2ac8b
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-14 09:49:06 -0700

    get the add functionality to work for total

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -26,18 +26,29 @@ class PurchasesController < ApplicationController
   end
 
   def edit
+    @products = Product.all
     @purchase = Purchase.find(params[:id])
     render :edit
   end
 
   def update
     @purchase = Purchase.find(params[:id])
-    if @purchase.update(purchase_params)
-      redirect_to purchases_path
+    if purchase_params["chk"]
+        param = {"total" => purchase_params["chk"].map(&:to_i).inject(0){|sum,x| sum + x }}
+        puts param
+        if @purchase.update(param)
+          redirect_to purchase_path
+        end
+    elsif @purchase.update(purchase_params)
+        puts purchase_params
+        # selected = params[:chk].inject(0){|sum,x| sum + x }
+        puts 'selected'
+        redirect_to purchases_path
     else 
-      render :edit
+        render :edit
     end
   end
+  
 
   def destroy
     @purchase = Purchase.find(params[:id])
@@ -45,9 +56,24 @@ class PurchasesController < ApplicationController
     redirect_to purchases_path
   end
 
+  def add_prices
+    total = 0
+    selected = params[:chk].inject(0){|sum,x| sum + x }
+    @purchase = Purchase.find(params[:id])
+    # products = Product.all
+    # products.each do |product|
+    #   if product.purchase_id == @purchase.id
+    #     total += product.price
+    #   end
+    # end
+    puts selected
+    @purchase.total = selected
+    self.update
+  end
+
   private
   def purchase_params
-    params.require(:purchase).permit(:name, :total, :created_at, :updated_at)
+    params.require(:purchase).permit(:name, :total, chk: [])
   end
   
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_one :purchase
+  validates :name, presence: true
+  validates :price, presence: true
 end
